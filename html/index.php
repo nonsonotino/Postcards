f9d6c9fdf536a0e70c0dce8a506389254b5867f2
Matteo Sartini - matteo.sartini8@studio.unibo.it <matteo.sartini8@studio.unibo.it>
2024-01-25 11:52:03 +0100

    php first attempt

---
+++ html/index.php
@@ -0,0 +1,24 @@
+<?php
+date_default_timezone_set('Europe/Rome');
+function printDate() {
+  echo get_date(D d, M Y); 
+}
+?>
+
+<!DOCTYPE html>
+<html lang="eng">
+
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<header>
+  <p><?php get_date() ?> </p>
+</header>
+
+<body>
+
+</body>
+
+</html>
