f2588b8d7ba8bdd6c2b56ffb36159ea0589b7581
nonsonotino <matteo.sartini26@gmail.com>
2024-01-25 16:23:09 +0100

    updated index

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 require_once "bootstrap.php";
 
+//Set template parameters
+$template_params["title"] = "Login";
+$template_params["base"] = "form_base.php";
+$template_params["type"] = "login_form.php";
 
+require("template/form_base.php");
 ?>
