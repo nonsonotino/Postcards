edd2a889ca3c874a5ae62673bd47293362cec567
nonsonotino <matteo.sartini26@gmail.com>
2024-01-26 18:13:48 +0100

    updated signup for bootstrap

--- template/login_form.php
+++ template/login_form.php
@@ -1,19 +1,21 @@
 <body class="bg-primary">
-<?php require("title_card.php"); ?>
-<form class="container d-flex justify-content-center" action="#">
-    <div class="">
-        <div class="d-flex justify-content-center">
-            <input class="textbox border-secondary form-control border-2 bg-primary rounded-pill text-secondary" id="username" name="username" type="text" placeholder="Username" maxlength="20" required>
+    <?php require("title_card.php"); ?>
+    <form class="container d-flex justify-content-center" action="#">
+        <div class="">
+            <div class="d-flex justify-content-center">
+                <input class="textbox border-secondary form-control border-2 bg-primary rounded-pill text-secondary"
+                    id="username" name="username" type="text" placeholder="Username" maxlength="20" required>
+            </div>
+            <div class="d-flex justify-content-center">
+                <input class="textbox border-secondary form-control border-2 bg-primary rounded-pill text-secondary"
+                    id="password" name="password" type="password" placeholder="Password" maxlength="20" required>
+            </div>
+            <div class="d-flex justify-content-end">
+                <input class="button rounded-pill btn btn-secondary text-primary" type="submit" value="Login">
+            </div>
         </div>
-        <div class="d-flex justify-content-center">
-            <input class="textbox border-secondary form-control border-2 bg-primary rounded-pill text-secondary" id="password" name="password" type="password" placeholder="Password" maxlength="20" required>
-        </div>
-        <div class="d-flex justify-content-end">
-            <input class="button rounded-pill btn btn-secondary text-primary" type="submit" value="Login">
-        </div>
-    </div>
-</form>
-<footer class="form-link fixed-bottom mb-4">
-    <p class="text-center text-secondary">New here? <a class="link-tertiary" href="#">Join us</a></p>
-</footer>
+    </form>
+    <footer class="form-link fixed-bottom mb-4">
+        <p class="text-center text-secondary">New here? <a class="link-tertiary" href="#">Join us</a></p>
+    </footer>
 </body>
--- template/signup_form.php
+++ template/signup_form.php
@@ -1,18 +1,31 @@
-<body>
-<form>
+<body class="bg-primary">
     <?php require("title_card.php"); ?>
-    <div class="form-group">
-        <input type="username" class="form-control" id="username" placeholder="Username">
-    </div>
-    <div class="form-group">
-        <input type="email" class="form-control" id="email" placeholder="Email">
-    </div>
-    <div class="form-group">
-        <input type="password" class="form-control" id="password" placeholder="Password">
-    </div>
-    <div class="form-group">
-        <input type="confirmPassword" class="form-control" id="confirmPassword" placeholder="Confirm password">
-    </div>
-    <button type="submit" class="btn btn-primary">Send</button>
-</form>
+    <form class="container d-flex justify-content-center" action="#">
+        <div>
+            <div class="d-flex justify-content-center">
+                <input class="textbox border-secondary form-control border-2 bg-primary rounded-pill text-secondary"
+                    id="username" name="username" type="text" placeholder="Username" maxlength="20" required>
+            </div>
+            <div class="d-flex justify-content-center">
+                <input class="textbox border-secondary form-control border-2 bg-primary rounded-pill text-secondary"
+                    id="email" name="email" type="text" placeholder="E-mail" maxlength="20" required>
+            </div>
+            <div class="d-flex justify-content-center">
+                <input class="textbox border-secondary form-control border-2 bg-primary rounded-pill text-secondary"
+                    id="password-first" name="password-first" type="password" placeholder="Password" maxlength="20"
+                    required>
+            </div>
+            <div class="d-flex justify-content-center">
+                <input class="textbox border-secondary form-control border-2 bg-primary rounded-pill text-secondary"
+                    id="password-second" name="password-second" type="password" placeholder="Confirm your password"
+                    maxlength="20" required>
+            </div>
+            <div class="d-flex justify-content-end">
+                <input class="button rounded-pill btn btn-secondary text-primary" type="submit" value="Sign in">
+            </div>
+        </div>
+    </form>
+    <footer class="form-link fixed-bottom mb-4">
+        <p class="text-center text-secondary">Already on board? <a class="link-tertiary" href="#">Login</a></p>
+    </footer>
 </body>
