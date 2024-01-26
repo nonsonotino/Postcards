91cb37c95c65899bb165caab0bfcb6603bcd4846
nonsonotino <matteo.sartini26@gmail.com>
2024-01-26 10:53:15 +0100

    updated bootstrap file

--- bootstrap.php
+++ bootstrap.php
@@ -3,5 +3,5 @@ session_start();
 define("ASSETS_DIR", "./assets/");
 require_once("util/utils.php");
 require_once("db/database.php");
-$dbh = new DatabaseHelper("localhost", "root", "", "blogtw", 3306);
+$dbh = new DatabaseHelper("localhost", "root", "", "postcards", 3306);
 ?>
