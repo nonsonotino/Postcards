fc91a5d5260feeac11596b6515f1c35dff3323e5
nonsonotino <matteo.sartini26@gmail.com>
2024-01-26 17:57:23 +0100

    updated login template to run bootstrap

--- template/base.php
+++ template/base.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/styles.css">
+    <!--<link rel="stylesheet" href="css/styles.css">-->
+    <link rel="stylesheet" href="css/newstyles.css">
     <link href='https://fonts.googleapis.com/css?family=Grape Nuts' rel='stylesheet'>
     <link href='https://fonts.googleapis.com/css?family=DM Serif Display' rel='stylesheet'>
     <link href='https://fonts.googleapis.com/css?family=DM Sans' rel='stylesheet'>
--- template/login_form.php
+++ template/login_form.php
@@ -1,11 +1,19 @@
 <body class="bg-primary">
 <?php require("title_card.php"); ?>
-<form class="container"action="#">
-    <input class="textbox form-control border bg-primary rounded-pill text-secondary" id="username" name="username" type="text" placeholder="Username" maxlength="20" required>
-    <input class="textbox border form-control bg-primary rounded-pill text-secondary" id="password" name="password" type="password" placeholder="Password" maxlength="20" required>
-    <input class="float-end button rounded-pill btn btn-secondary text-primary" type="submit" value="Login">
+<form class="container d-flex justify-content-center" action="#">
+    <div class="">
+        <div class="d-flex justify-content-center">
+            <input class="textbox border-secondary form-control border-2 bg-primary rounded-pill text-secondary" id="username" name="username" type="text" placeholder="Username" maxlength="20" required>
+        </div>
+        <div class="d-flex justify-content-center">
+            <input class="textbox border-secondary form-control border-2 bg-primary rounded-pill text-secondary" id="password" name="password" type="password" placeholder="Password" maxlength="20" required>
+        </div>
+        <div class="d-flex justify-content-end">
+            <input class="button rounded-pill btn btn-secondary text-primary" type="submit" value="Login">
+        </div>
+    </div>
 </form>
-<footer class="form-link">
-    <p class="text-center">New here? <a class="link-tertiary" href="#">Join us</a></p>
+<footer class="form-link fixed-bottom mb-4">
+    <p class="text-center text-secondary">New here? <a class="link-tertiary" href="#">Join us</a></p>
 </footer>
 </body>
