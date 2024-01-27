e0c4360cc00d9a0291412971d31dc64a278055a5
liviacardaccia02 <liviacardaccia99@gmail>
2024-01-27 17:12:59 +0100

    added a secure login script

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,11 @@
 <?php
-session_start();
+require_once("util/functions.php");
+sec_session_start();
+define("HOST", "localhost");
+define("USER", "sec_user");
+define("PASSWORD", "62L5LaaWysx4");
+define("DATABASE", "postcards");
 define("ASSETS_DIR", "./assets/");
-require_once("util/utils.php");
 require_once("db/database.php");
-$dbh = new DatabaseHelper("localhost", "root", "", "postcards", 3306);
-?>
+$dbh = new DatabaseHelper(HOST, USER, PASSWORD, DATABASE, 3306);
+?> 
