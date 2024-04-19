4a8f2cac2d0f5868059b832fa624266fd00c479a
nonsonotino <matteo.sartini26@gmail.com>
2024-04-19 16:44:43 +0200

    changed name to stylesheet

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-require_once "bootstrap.php";
+require_once("bootstrap.php");
 
 //Set template parameters
 $template_params["title"] = "Login";
-$template_params["page"] = "login_form.php";
+$template_params["page"] = "home.php";
 
 require("template/base.php");
 ?>
