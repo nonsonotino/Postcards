6fe3869d373a6fd694013ee0ab7a7fa6f977ee58
nonsonotino <matteo.sartini26@gmail.com>
2024-04-19 16:45:34 +0200

    updated base template

--- template/base.php
+++ template/base.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.css">
     <!--<link rel="stylesheet" href="css/styles.css">-->
-    <link rel="stylesheet" href="css/newstyles.css">
+    <link rel="stylesheet" href="css/styles.css">
     <link href='https://fonts.googleapis.com/css?family=Grape Nuts' rel='stylesheet'>
     <link href='https://fonts.googleapis.com/css?family=DM Serif Display' rel='stylesheet'>
     <link href='https://fonts.googleapis.com/css?family=DM Sans' rel='stylesheet'>
@@ -16,5 +16,6 @@
     </title>
 </head>
 <?php require($template_params["page"]); ?>
+
 
 </html>
