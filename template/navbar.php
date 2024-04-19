c19e21056f8db0d99b5dc0f057a9047bd432ae19
nonsonotino <matteo.sartini26@gmail.com>
2024-04-19 16:46:00 +0200

    updated navbar template

--- template/navbar.php
+++ template/navbar.php
@@ -1,11 +1,11 @@
-<div class="navbar bg-primary d-flex">
-    <a class=" align-self-start link-secondary justify-content-start" href="#">
-        <i class="fa-regular fa-bell"></i>
+<nav class="navbar navbar-expand-lg navbar-secondary bg-primary container-fluid py-4 d-flex justify-content-between">
+    <a class="link-secondary " href="#">
+        <i class="fa-regular fa-bell fa-xl"></i>
     </a>
-    <div class="date text-secondary align-self-center">
+    <div class="date text-secondary">
         <?php echo date("D d, M Y"); ?>
     </div>
-    <a class=" align-self-end link-secondary justify-content-start" href="#">
-        <i class="fa-solid fa-magnifying-glass"></i>
-    </a>
-</div>
+    <a class="link-secondary" href="#">
+        <i class="fa-solid fa-magnifying-glass fa-xl"></i>
+    </a> 
+</nav>
