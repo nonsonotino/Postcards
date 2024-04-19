5fe13f9950b2781828f1040594cce96a1c305e8e
liviacardaccia02 <liviacardaccia99@gmail>
2024-04-19 16:44:10 +0200

    login logic using javascript and ajax

--- template/login_form.php
+++ template/login_form.php
@@ -1,13 +1,11 @@
 <body class="bg-primary">
     <?php require("title_card.php"); ?>
     <?php
-    // Mostra il messaggio di errore se presente
     if (isset($template_params["error_message"])) {
         echo '<p style="color: red; text-align: center;">' . $template_params["error_message"] . '</p>';
-        // Rimuovi il messaggio di errore dalla sessione dopo averlo mostrato
     }
     ?>
-    <form class="container d-flex justify-content-center" method="POST" action="login.php">
+    <form class="container d-flex justify-content-center" method="POST" action="/ajax/login.php">
         <div class="">
             <div class="d-flex justify-content-center">
                 <input class="textbox border-secondary form-control border-2 bg-primary rounded-pill text-secondary"
@@ -18,11 +16,12 @@
                     id="password" name="password" type="password" placeholder="Password" maxlength="20" required>
             </div>
             <div class="d-flex justify-content-end">
-                <input class="button rounded-pill btn btn-secondary text-primary" type="submit" value="Login">
+                <input id="login" class="button rounded-pill btn btn-secondary text-primary" type="submit" value="Login">
             </div>
         </div>
     </form>
     <footer class="form-link fixed-bottom mb-4">
         <p class="text-center text-secondary">New here? <a class="link-tertiary" href="#">Join us</a></p>
     </footer>
+    <script src="js/login.js"></script>
 </body>
