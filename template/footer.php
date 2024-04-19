4528cc1f07108e7136725c89d6f0d52f03c66f3c
nonsonotino <matteo.sartini26@gmail.com>
2024-04-19 16:46:39 +0200

    added footer template

---
+++ template/footer.php
@@ -0,0 +1,11 @@
+<footer class="fixed-bottom py-3 m-3 d-flex justify-content-between border-top border-3 border-primary">
+    <a class="link-primary ml-3" href="#">
+        <i class="fa-regular fa-envelope fa-xl"></i>
+    </a>
+    <a class="link-primary " href="#">
+        <i class="fa-regular fa-paper-plane fa-xl"></i>
+    </a>
+    <a class="link-primary mr-3" href="#">
+        <img src="assets/default.jpg" class="img-fluid rounded-pill border border-3 border-primary" alt="Immagine profilo">
+    </a>
+</footer>
