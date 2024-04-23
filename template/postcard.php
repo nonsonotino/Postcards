215804b256222e376dd83c63983fb8cab580bcca
nonsonotino <matteo.sartini26@gmail.com>
2024-04-23 14:11:06 +0200

    added test postcard with rotation

---
+++ template/postcard.php
@@ -0,0 +1,15 @@
+
+<div class="postcard-container container-fluid d-flex justify-content-center">
+<div class="flip-card align-self-center ">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="assets/landscape.jpeg" alt="Avatar" style="width:300px;height:300px;">
+    </div>
+    <div class="flip-card-back">
+      <h1>John Doe</h1>
+      <p>Architect & Engineer</p>
+      <p>We love that guy</p>
+    </div>
+  </div>
+</div>
+</div>
