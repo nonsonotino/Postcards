3d72c5fc4dd056dc458a36187af2b481401853b1
nonsonotino <matteo.sartini26@gmail.com>
2024-04-23 14:11:31 +0200

    updated homepage with new postcard element

--- template/home.php
+++ template/home.php
@@ -1,4 +1,5 @@
-<body class="bg-secondary">
+<body class="d-flex bg-secondary">
     <?php require("navbar.php") ?>
+    <?php require("postcard.php") ?>
     <?php require("footer.php") ?>
 </body>
