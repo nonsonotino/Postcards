ad746238e02a404ff15cffc78b84efe491aa850c
nonsonotino <matteo.sartini26@gmail.com>
2024-05-06 13:09:54 +0200

    updated postcard template with background and work in progress scaling

--- template/postcard.php
+++ template/postcard.php
@@ -1,15 +1,12 @@
-
-<div class="postcard-container container-fluid d-flex justify-content-center">
-<div class="flip-card align-self-center ">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-      <img src="assets/landscape.jpeg" alt="Avatar" style="width:300px;height:300px;">
-    </div>
-    <div class="flip-card-back">
-      <h1>John Doe</h1>
-      <p>Architect & Engineer</p>
-      <p>We love that guy</p>
+<div class="card align-self-center bg-secondary">
+  <div class="card-side front bg-white p-2">
+    <img class="postcard-image w-100 h-100 align-self-center" src="assets/landscape.jpeg" alt="Postcard Front">
+  </div>
+  <div class="card-side back">
+    <div class="postcard-back w-100 h-100 p-2">
+      <div class="bg-white h-100 w-100">
+        
+      </div>
     </div>
   </div>
-</div>
 </div>
