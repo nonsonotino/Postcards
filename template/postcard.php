60b4cf4aabee9656a38a57a78425ed085186e6d9
nonsonotino <matteo.sartini26@gmail.com>
2024-05-07 11:35:39 +0200

    updated postcard class names

--- template/postcard.php
+++ template/postcard.php
@@ -3,9 +3,14 @@
     <img class="postcard-image w-100 h-100 align-self-center" src="assets/landscape.jpeg" alt="Postcard Front">
   </div>
   <div class="card-side back">
-    <div class="postcard-back w-100 h-100 p-2">
-      <div class="bg-white h-100 w-100">
-        
+    <div class="postcard-background w-100 h-100 p-2">
+      <div class="postcard-back bg-white h-100 w-100 d-flex">
+        <div class="postcard-description">
+          <p class="text-start text-wrap w-100 h-100">Hi, look at these beautiful moubtains! rdftyghjiklkjhgufydtsedrtyghoijlèkjiogyufdrtyftuhoij jkjlkh</p>
+        </div>
+        <div class="postcard-info">
+          <img src="assets/profile_default.jpg" class="info-image  rounded-pill border border-3 border-primary" alt="Immagine profilo">
+        </div>
       </div>
     </div>
   </div>
