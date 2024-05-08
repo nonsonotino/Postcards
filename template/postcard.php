3214572ce69ac5fa9129d95b628ab2d7ac8ca5a6
nonsonotino <matteo.sartini26@gmail.com>
2024-05-08 13:31:52 +0200

    added postcard information side

--- template/postcard.php
+++ template/postcard.php
@@ -5,11 +5,33 @@
   <div class="card-side back">
     <div class="postcard-background w-100 h-100 p-2">
       <div class="postcard-back bg-white h-100 w-100 d-flex">
+
         <div class="postcard-description">
-          <p class="text-start text-wrap w-100 h-100">Hi, look at these beautiful moubtains! rdftyghjiklkjhgufydtsedrtyghoijlèkjiogyufdrtyftuhoij jkjlkh</p>
+          <p class="text-start text-wrap w-100 h-100">Hi, look at these beautiful mountains!
+            rdftyghjiklkjhgufydtsedrtyghoijlèkjiogyufdrtyftuhoij jkjlkh</p>
         </div>
-        <div class="postcard-info">
-          <img src="assets/profile_default.jpg" class="info-image  rounded-pill border border-3 border-primary" alt="Immagine profilo">
+
+        <div class="postcard-info d-flex align-items-start">
+          <img src="assets/profile_default.jpg" class="info-image  rounded-pill border border-3 border-primary ratio ratio-1x1 mb-3"
+              alt="Immagine profilo">
+          <div class="d-flex flex-row justify-content-start mb-2 w-100">
+            <a class="info-symbol link-dark" href="#">
+              <i class="fa-solid fa-user"></i>
+            </a>
+            <a class="info-text link-dark" href="#">username</a>
+          </div>
+          <div class="d-flex flex-row justify-content-start mb-2 w-100">
+            <a class="info-symbol link-dark" href="#">
+              <i class="fa-solid fa-location-dot"></i>
+            </a>
+            <a class="info-text link-dark" href="#">Location, Place</a>
+          </div>
+          <div class="d-flex flex-row justify-content-start mb-2 w-100">
+            <div class="info-symbol text-dark">
+              <i class="fa-regular fa-calendar"></i>
+            </div>
+            <p class="info-text">08/05/2024</p>
+          </div>
         </div>
       </div>
     </div>
