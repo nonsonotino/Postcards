3b7c8cb28ec60dcc7397e36a9b9908499a24264b
nonsonotino <matteo.sartini26@gmail.com>
2024-05-09 14:53:48 +0200

    added first iteration of generic profile page

---
+++ template/profile.php
@@ -0,0 +1,13 @@
+<body class="d-flex flex-column align-items-start justify-content-center bg-secondary">
+    <?php require ("profile_header.php") ?>
+        <div class="postcard-profile-container d-flex flex-wrap justify-content-center
+    ">
+        <?php
+        for ($i = 0; $i < 5; $i++) {
+             require ("profile-postcard.php");
+        }
+        ?>
+        </div>
+
+    <?php require ("footer.php") ?>
+</body>
---
+++ template/profile_header.php
@@ -0,0 +1,13 @@
+<header
+    class="d-flex flex-row m-3 mt-0 pt-4 border-bottom border-primary border-3 border-primary fixed-top justify-content-around bg-secondary">
+    <div class="d-flex flex-column">
+        <img src="assets/profile_default.jpg" alt="Immagine profilo"
+            class=" profile-image rounded-pill border border-4 border-primary ratio ratio-1x1 mb-1 mt-1">
+        <p>@nomeUtente</p>
+    </div>
+    <div class="d-flex flex-column align-items-center">
+        <p class="m-0">11</p>
+        <p class="mb-2">penfriends</p>
+        <button class="btn btn-primary btn-sm">add to friends</button>
+    </div>
+</header>
