e6a6112e807a194a8ab4dddf1b6bbcad207109b7
nonsonotino <matteo.sartini26@gmail.com>
2024-05-10 14:43:01 +0200

    fixed notifications page to work in flexbox

---
+++ template/notification.php
@@ -0,0 +1,11 @@
+<div class="d-flex flex-row border-bottom border-2 border-primary p-2 justify-content-start align-items-center align">
+    <a class="link-primary me-2" href="#">
+        <img src="assets/profile_default.jpg" class="footer-image  rounded-pill border border-3 border-primary" alt="Immagine profilo">
+    </a>
+    <a class="link-primary me-2" href="#">
+        UserUsername
+    </a>
+    <p class="m-0">
+        Liked your postcard!
+    </p>
+</div>
--- template/notifications_page.php
+++ template/notifications_page.php
@@ -1,6 +1,12 @@
-<body class="bg-secondary">
+<body class="bg-secondary d-flex justify-content-start flex-column">
     <?php require("navbar.php") ?>
-    
+    <div class="notifications-scroll d-flex flex-column overflow-scroll h-100">
+        <?php
+        for ($i = 0; $i < 100; $i++) {
+            require("notification.php");
+        }
+        ?>
+    </div>
     <?php require("footer.php") ?>
     
 </body>
