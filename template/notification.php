e2bb56b49f3c622fba31f68770b194bd882434e7
nonsonotino <matteo.sartini26@gmail.com>
2024-05-10 16:39:36 +0200

    fixed margin errors in footer and notifications

--- template/notification.php
+++ template/notification.php
@@ -1,4 +1,4 @@
-<div class="d-flex flex-row border-bottom border-2 border-primary p-2 justify-content-start align-items-center align">
+<div class="d-flex flex-row border-bottom border-2 border-primary p-2 justify-content-start align-items-center mx-3 py-2">
     <a class="link-primary me-2" href="#">
         <img src="assets/profile_default.jpg" class="footer-image  rounded-pill border border-3 border-primary" alt="Immagine profilo">
     </a>
