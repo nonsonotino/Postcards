3df1ca64c2922f9a8baa63e20b3de52162e24251
nonsonotino <matteo.sartini26@gmail.com>
2024-05-15 14:36:36 +0200

    added templates for post creation page

---
+++ template/creation_header.php
@@ -0,0 +1,3 @@
+<div class="d-flex flex-row justify-content-center align-items-center border-bottom border-3 border-primary mx-3">
+    <p class="creation-title fs-1 text-primary m-0">Write your postcard</p>
+</div>
