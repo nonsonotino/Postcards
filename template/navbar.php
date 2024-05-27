bae749fe7016a264b5568f4adefcfb311d0826f7
liviacardaccia02 <liviacardaccia99@gmail>
2024-05-27 20:45:27 +0200

    linked notification page

--- template/navbar.php
+++ template/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-secondary bg-primary container-fluid py-4 d-flex justify-content-between">
-    <a class="link-secondary " href="#">
+    <a class="link-secondary " href="notifications.php">
         <i class="fa-regular fa-bell fa-xl"></i>
     </a>
     <div class="date text-secondary">
