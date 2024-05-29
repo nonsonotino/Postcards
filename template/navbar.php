264f52211ff41cab7f03844c73b43586a72fca07
nonsonotino <matteo.sartini26@gmail.com>
2024-05-29 20:43:37 +0200

    added searchbar to navabr

--- template/navbar.php
+++ template/navbar.php
@@ -2,10 +2,11 @@
     <a class="link-secondary " href="notifications.php">
         <i class="fa-regular fa-bell fa-xl"></i>
     </a>
-    <div class="date text-secondary">
+    <div class="date text-secondary align-self-center">
         <?php echo date("D d, M Y"); ?>
     </div>
-    <a class="link-secondary" href="search.php">
-        <i class="fa-solid fa-magnifying-glass fa-xl"></i>
-    </a>
+    <form class="d-flex flex-line">
+      <input class="" type="search" placeholder="Search">
+      <button class="btn bg-primary" type="submit"><i class="fa-solid fa-magnifying-glass fa-xl"></i></i></button>
+    </form>
 </nav>
