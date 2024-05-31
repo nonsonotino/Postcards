b69e5480c72e0d4536589b75c737c11efe444afe
liviacardaccia02 <liviacardaccia99@gmail>
2024-05-31 14:36:59 +0200

    index now redirecting both on home and login

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,15 @@
 <?php
-session_start();
 define("ASSETS_DIR", "./assets/");
-require_once("db/database.php");
-require_once("util/functions.php");
+require_once ("db/database.php");
+require_once ("util/functions.php");
 $dbh = new DatabaseHelper("localhost", "root", "", "postcards", 3306);
-?> 
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+if (!isUserLoggedIn() && basename($_SERVER['PHP_SELF']) !== 'login.php') {
+    header("Location: login.php");
+    exit();
+}
+?>
