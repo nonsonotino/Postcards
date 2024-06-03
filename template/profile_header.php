7994c785ca051e73d73f2333c7e76208dc3e965a
nonsonotino <matteo.sartini26@gmail.com>
2024-06-03 14:54:30 +0200

    fixed profile header layout

--- template/profile_header.php
+++ template/profile_header.php
@@ -1,5 +1,5 @@
 <header
-    class="d-flex flex-row m-3 mt-0 mb-0 pt-4 pb-3 border-bottom border-primary border-3 border-primary justify-content-around bg-secondary ">
+    class="d-flex flex-row m-3 mt-0 mb-0 pt-4 pb-3 border-bottom border-primary border-3 border-primary bg-secondary justify-content-around">
     <div class="d-flex flex-column">
         <img src="assets/profile_default.jpg" alt="Immagine profilo"
             class=" profile-image rounded-pill border border-4 border-primary ratio ratio-1x1 mb-1 mt-1">
@@ -8,12 +8,12 @@
     <div class="d-flex flex-column align-items-center">
         <p class="m-0"><?php echo count($friends) ?></p>
         <p class="mb-2">penfriends</p>
-        <button id="addToFriends" class="btn btn-primary btn-sm">add to friends</button>
-        <button id="removeFromFriends" class="btn btn-primary btn-sm bg-secondary text-dark border-2 border-primary"
+        <button id="addToFriends" class="friend-button btn btn-primary btn-sm">add to friends</button>
+        <button id="removeFromFriends" class="friend-button btn btn-primary btn-sm bg-secondary text-dark border-2 border-primary"
             hidden>remove from
             friends</button>
     </div>
-    <div class="d-flex flex-column">
+    <div class="position-absolute top-0 end-0 d-flex flex-column m-1">
         <button id="logout" class="btn btn-primary btn-sm">Logout</button>
     </div>
 </header>
