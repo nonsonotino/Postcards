ab604a23951ddd349ba96aed92c604eb1924bc7d
nonsonotino <matteo.sartini26@gmail.com>
2024-06-03 14:14:46 +0200

    added resizing for info text and symbols

--- template/comment_page.php
+++ template/comment_page.php
@@ -7,5 +7,9 @@
         }
         ?>
     </div>
+    <form class="d-flex flex-cloumn p-2 border-top border-2 border-primary mx-3">
+      <input class="form-control bg-secondary border border-1 border-primary me-2" type="text" placeholder="Write something..." aria-label="Search">
+      <button class="btn border border-1 border-primary bg-primary text-secondary" type="submit">Send</button>
+    </form>
     <?php require("footer.php") ?>
 </body>
