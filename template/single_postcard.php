a513f6f6a3b999623dd7b4df5546fd6080197d64
nonsonotino <matteo.sartini26@gmail.com>
2024-06-03 14:54:40 +0200

    added single postcard page

---
+++ template/single_postcard.php
@@ -0,0 +1,6 @@
+<body class="d-flex flex-column bg-secondary">
+    <?php require ("navbar.php") ?>
+    <?php require ("postcard.php") ?> <!--add selected postcard--!>
+    <?php require ("footer.php") ?>
+    <script src="js/home.js"></script>
+</body>
