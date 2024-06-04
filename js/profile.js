5bc141b9d6f99e906a01def21c072a067268ddb2
liviacardaccia02 <liviacardaccia99@gmail>
2024-06-04 17:00:55 +0200

    edit button now correctly redirecting

--- js/profile.js
+++ js/profile.js
@@ -1,25 +1,26 @@
 window.onload = function () {
-    let addButton = document.getElementById("addToFriends");
+    //let addButton = document.getElementById("addToFriends");
     let removeButton = document.getElementById("removeFromFriends");
+    let editButton = document.getElementById("editProfile");
     let logoutButton = document.getElementById("logout");
 
-    addButton.addEventListener("click", function (e) {
-        e.preventDefault();
-        addButton.hidden = true;
-        removeButton.hidden = false;
-        $.ajax({
-            url: "ajax/add_friend.php",
-            type: "POST",
-            success: function (response) {
-                console.log(response);
-                if (response.trim() == "success") {
-                    console.log("Friend added");
-                } else {
-                    console.log("Friend not added");
-                }
-            },
-        });
-    });
+    // addButton.addEventListener("click", function (e) {
+    //     e.preventDefault();
+    //     addButton.hidden = true;
+    //     removeButton.hidden = false;
+    //     $.ajax({
+    //         url: "ajax/add_friend.php",
+    //         type: "POST",
+    //         success: function (response) {
+    //             console.log(response);
+    //             if (response.trim() == "success") {
+    //                 console.log("Friend added");
+    //             } else {
+    //                 console.log("Friend not added");
+    //             }
+    //         },
+    //     });
+    // });
 
     removeButton.addEventListener("click", function (e) {
         e.preventDefault();
@@ -37,6 +38,12 @@ window.onload = function () {
                 }
             },
         });
+    });
+
+    editButton.addEventListener("click", function (e) {
+        e.preventDefault();
+        console.log("pressed");
+        window.location.href = "/Postcards/edit_profile.php";
     });
 
     logoutButton.addEventListener("click", function (e) {
--- template/edit_profile.php
+++ template/edit_profile.php
@@ -2,9 +2,8 @@
     <div class="d-flex flex-row justify-content-center align-items-center border-bottom border-3 border-primary mx-3">
         <p class="creation-title fs-1 text-primary m-0">Edit your profile picture</p>
     </div>
-    <script src="js/profile_creation.js"></script>
     <div class="post-form w-50 h-100 align-self-center overflow-scroll py-3">
-        <form id="profileCreationForm" method="POST" action="/ajax/profile_creation.php" class="d-flex flex-column">
+        <form id="editProfileForm" method="POST" action="#" class="d-flex flex-column">
             <label for="image-input" class="form-label fs-5 text-primary">Choose a picture:</label>
             <div class="bg-white p-2 selected-image border border-primary">
                 <img id="defaultImage" src="assets/profile_default.jpg" alt="selected image"
@@ -19,4 +18,4 @@
     </div>
     <?php require ("footer.php") ?>
 </body>
-<script src="/Postcards/js/profile_creation.js"></script>
+<script src="/Postcards/js/edit_profile.js"></script>
