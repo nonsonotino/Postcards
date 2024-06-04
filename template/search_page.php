22549cc513de162536a0e46b4505f98c82479148
nonsonotino <matteo.sartini26@gmail.com>
2024-06-04 14:45:24 +0200

    fixed text color in search page

--- template/search_page.php
+++ template/search_page.php
@@ -2,7 +2,7 @@
     <?php require ("navbar.php") ?>
     <form class="d-flex flex-cloumn p-2 border-bottom border-2 border-primary mx-2">
       <input class="form-control bg-secondary border border-1 border-primary me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn border border-1 border-primary bg-primary text-secondary" type="submit">Search</button>
+      <button class="btn border border-1 border-primary bg-primary text-white" type="submit">Search</button>
     </form>
     <div class="comments-scroll h-100 d-flex flex-column overflow-scroll h-100">
         <?php
