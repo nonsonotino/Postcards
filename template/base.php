c88731a93ff23830a19a97b3646feb2671ab7446
liviacardaccia02 <liviacardaccia99@gmail>
2024-06-05 22:51:23 +0200

    minor fix

--- template/base.php
+++ template/base.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/styles.css">    
+    <link rel="stylesheet" href="css/styles.css">
     <link href='https://fonts.googleapis.com/css?family=Grape Nuts' rel='stylesheet'>
     <link href='https://fonts.googleapis.com/css?family=DM Serif Display' rel='stylesheet'>
     <link href='https://fonts.googleapis.com/css?family=DM Sans' rel='stylesheet'>
@@ -15,7 +15,7 @@
         <?php echo $template_params["title"]; ?>
     </title>
 </head>
-<?php require($template_params["page"]); ?>
+<?php require ($template_params["page"]); ?>
 
 
 </html>
