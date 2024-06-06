077a66093f1cd2982b1f72a5582d576502894384
liviacardaccia02 <liviacardaccia99@gmail>
2024-06-06 23:14:37 +0200

    logout fixed

--- ajax/logout.php
+++ ajax/logout.php
@@ -1,18 +1,11 @@
 <?php
 session_start();
 
-if (session_unset()) {
-    if (session_destroy()) {
-        // header("Location: login.php");
-        // exit();
-        echo "success";
-    } else {
-        $errorMessage = "Error! Unable to destroy the session.";
-    }
-}
-
-if (isset($errorMessage)) {
+session_unset();
+if (session_destroy()) {
+    echo "success";
+} else {
+    $errorMessage = "Error! Unable to destroy the session.";
     echo json_encode(["error" => $errorMessage]);
 }
-
 ?>
