d9b42c2edd1d5cee788f01d2c2895b405ea9a5b5
nonsonotino <matteo.sartini26@gmail.com>
2024-06-06 18:26:59 +0200

    updated followers stats styles

--- template/profile_header.php
+++ template/profile_header.php
@@ -1,18 +1,21 @@
-<header
-    class="d-flex flex-row m-3 mt-0 mb-0 pt-4 pb-3 border-bottom border-primary border-3 border-primary bg-secondary justify-content-around">
+<header class="d-flex flex-row m-3 mt-0 mb-0 pt-4 pb-3 border-bottom border-primary border-3 border-primary bg-secondary justify-content-around">
     <div class="d-flex flex-column">
-        <img src="<?= str_replace("../", "", $profile["profilePicture"]); ?>" alt="Profile picture"
-            class=" profile-image rounded-pill border border-4 border-primary ratio ratio-1x1 mb-1 mt-1">
+        <img src="<?= str_replace("../", "", $profile["profilePicture"]); ?>" alt="Profile picture" class=" profile-image rounded-pill border border-4 border-primary ratio ratio-1x1 mb-1 mt-1">
         <p class="text-center"><?php echo $profile["username"] ?></p>
     </div>
     <div class="d-flex flex-column align-items-center">
-        <p class="m-0"><?php echo $profile["friends"] ?></p>
-        <p class="mb-2">penfriends you have</p>
-        <p class="m-0"><?php echo $profile["friendsFollowed"] ?></p>
-        <p class="mb-2">penfriends you follow</p>
+        <div class="d-flex flex-row">
+            <div class="d-flex flex-column align-items-center me-3">
+                <p class="m-0"><?php echo $profile["friends"] ?></p>
+                <p class="mb-2">followers</p>
+            </div>
+            <div class="d-flex flex-column align-items-center">
+                <p class="m-0"><?php echo $profile["friendsFollowed"] ?></p>
+                <p class="mb-2">following</p>
+            </div>
+        </div>
         <button id="editProfile" class="friend-button btn btn-primary btn-sm">edit your profile</button>
-        <button id="removeFromFriends"
-            class="friend-button btn btn-primary btn-sm bg-secondary text-dark border-2 border-primary" hidden>remove
+        <button id="removeFromFriends" class="friend-button btn btn-primary btn-sm bg-secondary text-dark border-2 border-primary" hidden>remove
             from
             friends</button>
     </div>
