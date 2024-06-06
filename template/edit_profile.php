f09be00dc157f19adaa304e80afa3bba54a6c280
nonsonotino <matteo.sartini26@gmail.com>
2024-06-06 15:09:00 +0200

    fixed profile edit style

--- template/edit_profile.php
+++ template/edit_profile.php
@@ -3,16 +3,14 @@
         <p class="creation-title fs-1 text-primary m-0">Edit your profile picture</p>
     </div>
     <div class="post-form w-50 h-100 align-self-center overflow-scroll py-3">
-        <form id="editProfileForm" method="POST" action="#" class="d-flex flex-column">
+        <form id="editProfileForm" method="POST" action="#" class="d-flex flex-column align-items-center">
             <label for="image-input" class="form-label fs-5 text-primary">Choose a picture:</label>
-            <div class="bg-white p-2 selected-image border border-primary">
-                <img id="defaultImage" src="assets/profile_default.jpg" alt="selected image"
-                    class="postcard-image w-100">
-            </div>
+            <img id="defaultImage" src="assets/profile_default.jpg" alt="selected image"
+                    class="w-50 profile-edit-image rounded-pill border border-3 border-primary">
             <input type="file" class="form-control d-none" id="profileImage" name="profileImage"
                 accept="image/jpg, image/jpeg, image/png, image/gif" />
             <span id="errorText" class="text-danger fs-7" hidden></span>
-            <button id="submit" type="submit" class="btn btn-primary mt-3 w-25 form-button align-self-end">Edit</button>
+            <button id="submit" type="submit" class="btn btn-primary mt-3 w-25 form-button">Edit</button>
         </form>
 
     </div>
