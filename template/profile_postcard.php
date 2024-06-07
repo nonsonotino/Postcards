a99e8d9ca8ea30c47450ba91f6921dcf54db21a0
liviacardaccia02 <liviacardaccia99@gmail>
2024-06-07 17:49:20 +0200

    corrected image profile

--- template/profile_postcard.php
+++ template/profile_postcard.php
@@ -1,6 +1,6 @@
 <div class="profile-postcard bg-secondary m-2">
   <div class="bg-white p-2">
-    <a href="single_postcard.php?postcardId=<?= $postcard["idPostCard"] ?>">
+    <a href="single_postcard.php?postcardId=<?= $postcard["idPostCard"] ?>&username=<?= $postcard["username"] ?>">
       <img class="postcard-image w-100 h-100 align-self-center" src="<?= str_replace("../", "", $postcard['image']) ?>"
         alt="Postcard Front">
     </a>
