49077b8b5134e5db02f44262ac3d4269bf98711b
liviacardaccia02 <liviacardaccia99@gmail>
2024-06-07 17:38:43 +0200

    arrow now redirecting to previous page

--- template/comment_page.php
+++ template/comment_page.php
@@ -1,6 +1,6 @@
 <body class="bg-secondary d-flex flex-column">
     <div class="d-flex flex-row justify-content-start align-items-center border-bottom border-3 border-primary mx-3">
-        <a id="arrowBack" href="#" class="link-primary me-3 py-3">
+        <a id="arrowBack" href="home.php" class="link-primary me-3 py-3">
             <i class="fa-solid fa-arrow-left fs-1"></i>
         </a>
         <p class="comments-title fs-1 text-primary m-0">Comments</p>
