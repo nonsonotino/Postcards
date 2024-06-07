776e7db263e456edd079b84d281df6c2ba57cd7f
liviacardaccia02 <liviacardaccia99@gmail>
2024-06-07 17:39:13 +0200

    corrected logic for the arrow and logout button

--- template/profile_header.php
+++ template/profile_header.php
@@ -1,6 +1,15 @@
-<header class="d-flex flex-row m-3 mt-0 mb-0 pt-4 pb-3 border-bottom border-primary border-3 border-primary bg-secondary justify-content-around">
+<header
+    class="d-flex flex-row m-3 mt-0 mb-0 pt-4 pb-3 border-bottom border-primary border-3 border-primary bg-secondary justify-content-around position-relative">
+    <?php if ($profile["username"] != $_SESSION["username"]): ?>
+        <div class="position-absolute top-0 start-0 d-flex flex-column">
+            <a id="arrowBack" href="home.php" class="link-primary me-3 py-3">
+                <i class="fa-solid fa-arrow-left fs-1"></i>
+            </a>
+        </div>
+    <?php endif; ?>
     <div class="d-flex flex-column">
-        <img src="<?= str_replace("../", "", $profile["profilePicture"]); ?>" alt="Profile picture" class=" profile-image rounded-pill border border-4 border-primary ratio ratio-1x1 mb-1 mt-1">
+        <img src="<?= str_replace("../", "", $profile["profilePicture"]); ?>" alt="Profile picture"
+            class=" profile-image rounded-pill border border-4 border-primary ratio ratio-1x1 mb-1 mt-1">
         <p class="text-center"><?php echo $profile["username"] ?></p>
     </div>
     <input type="hidden" id="profileUsername" name="profileUsername" value="<?php echo $profile["username"] ?>" />
@@ -16,22 +25,24 @@
                 <p id="labelFriendsFollowed" class="mb-2">following</p>
             </div>
         </div>
-        <?php if ($profile["username"] == $_SESSION["username"]) : ?>
+        <?php if ($profile["username"] == $_SESSION["username"]): ?>
             <button id="editProfile" class="friend-button btn btn-primary btn-sm">edit your profile</button>
-        <?php else : ?>
-            <?php if ($template_params["isFriend"]) : ?>
-                <button id="removeFriend" class="friend-button btn btn-primary btn-sm bg-secondary text-dark border-2 border-primary">remove from
+            <div class="position-absolute top-0 end-0 d-flex flex-column m-1">
+                <button id="logout" class="btn btn-primary btn-sm">Logout</button>
+            </div>
+        <?php else: ?>
+            <?php if ($template_params["isFriend"]): ?>
+                <button id="removeFriend"
+                    class="friend-button btn btn-primary btn-sm bg-secondary text-dark border-2 border-primary">remove from
                     friends</button>
                 <button id="addFriend" class="friend-button btn btn-primary btn-sm" hidden>add to friends</button>
-            <?php else : ?>
+            <?php else: ?>
                 <button id="addFriend" class="friend-button btn btn-primary btn-sm">add to friends</button>
-                <button id="removeFriend" class="friend-button btn btn-primary btn-sm bg-secondary text-dark border-2 border-primary" hidden>remove
+                <button id="removeFriend"
+                    class="friend-button btn btn-primary btn-sm bg-secondary text-dark border-2 border-primary" hidden>remove
                     from friends</button>
             <?php endif; ?>
         <?php endif; ?>
         <span id="errorText" class="text-danger fs-7" hidden></span>
-    </div>
-    <div class="position-absolute top-0 end-0 d-flex flex-column m-1">
-        <button id="logout" class="btn btn-primary btn-sm">Logout</button>
     </div>
 </header>
